66745b33e5aacde03f2db22a6675b3ad1eea3991
Marcin Radziszewski <cinos1994@hotmail.com>
2017-11-24 12:56:21 +0100

    add project

---
+++ NUnitDemo.Tests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitDemo.Tests
+{
+        [TestFixture]
+        public class CalculatorTests
+        {
+            [Test]
+            public void Add_Always_ReturnsExpectedResult()
+            {
+                var systemUnderTest = new Calculator();
+                Assert.That(systemUnderTest.Add(1, 2), Is.EqualTo(3));
+            }
+        }
+}
---
+++ NUnitDemo/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitDemo
+{
+    public class Calculator
+    {
+        public int Add(int lhs, int rhs)
+        {
+            return lhs + rhs;
+        }
+    }
+}
